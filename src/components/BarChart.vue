44f57b231397d47a1c1a8305b0724b6e0d20e8e0
jiun12345 <jiunkim0921@gmail.com>
2026-04-08 13:56:38 +0900

    기능 및 디자인 구현 완료

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -19,7 +19,6 @@ ChartJS.register(
   LinearScale,
 );
 
-// 부모로부터 데이터를 전달받음
 defineProps({
   chartData: {
     type: Object,
@@ -27,10 +26,51 @@ defineProps({
   },
 });
 
+/**
+ * [디자인 포인트 1] 차트 옵션 설정
+ */
 const chartOptions = {
   responsive: true,
   maintainAspectRatio: false,
-  scales: { y: { beginAtZero: true } },
+  plugins: {
+    // 범례 디자인 (INCOME, EXPENSE)
+    legend: {
+      display: true,
+      position: 'top',
+      align: 'end', // 우측 상단 정렬
+      labels: {
+        color: '#aaaaaa', // 글자 색상
+        usePointStyle: true, // 점 형태의 범례
+        pointStyle: 'circle',
+        boxWidth: 8,
+        padding: 20,
+      },
+    },
+    // 풍선 도움말 디자인
+    tooltip: {
+      backgroundColor: '#333333',
+      titleColor: '#ffffff',
+      bodyColor: '#dddddd',
+      cornerRadius: 8,
+      padding: 12,
+    },
+  },
+  // [디자인 포인트 2] 축 스타일링
+  scales: {
+    x: {
+      grid: {
+        display: false, // X축 그리드 숨김
+      },
+      ticks: {
+        color: '#666666', // 월별 라벨 색상 (01월...)
+        font: { size: 12 },
+      },
+    },
+    y: {
+      display: false, // Y축 숨김 (디자인 참고)
+      grid: { display: false },
+    },
+  },
 };
 </script>
 
